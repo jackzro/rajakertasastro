5e7029960a9afcf9921d639515a617448605cf95
Sujackson <su_jackson99@yahoo.com>
2024-06-30 20:12:10 +0700

    oo

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import node from "@astrojs/node";
 import partytown from "@astrojs/partytown";
-import basicSsl from "@vitejs/plugin-basic-ssl";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
@@ -25,12 +24,6 @@ export default defineConfig({
   adapter: node({
     mode: "standalone",
   }),
-  vite: {
-    plugins: [basicSsl()],
-    // server: {
-    //   https: true,
-    //   port: 3000,
-    // },
-  },
+
   site: "https://rajakertas.id",
 });
