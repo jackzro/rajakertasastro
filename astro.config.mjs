4ab472d79ed6004141493c16c63444df585af850
Sujackson <su_jackson99@yahoo.com>
2024-07-10 08:31:02 +0700

    vite

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,12 +19,6 @@ export default defineConfig({
       },
     }),
   ],
-  vite: {
-    ssr: {
-      // Example: Force a broken package to skip SSR processing, if needed
-      external: ["broken-npm-package"],
-    },
-  },
   output: "server",
   adapter: node({
     mode: "standalone",
